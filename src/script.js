69f347c08505e89cf0621dd2be4eac7fe7fcfc41
Nirmal Prabhu <mpnirmal44@gmail.com>
2024-06-10 21:05:18 -0400

    fluid mouse pointer added

--- src/script.js
+++ src/script.js
@@ -1,3 +1,57 @@
+
+//gorgeous mouse pointer animation
+
+const canvas = document.getElementById('renderSurface');
+
+let myFluid = new Fluid(canvas);
+
+
+
+myFluid.mapBehaviors({
+  sim_resolution: 128,
+  dye_resolution: 1024,
+
+  paused: false,
+  embedded_dither: true,
+
+  dissipation: .97,
+  velocity: .98,
+  pressure: .8,
+  pressure_iteration: 20,
+  curl: 0,
+  emitter_size: 0.5,
+
+  render_shaders: true,
+  multi_color: true,
+
+  render_bloom: false,
+  bloom_iterations: 8,
+  bloom_resolution: 256,
+  intensity: 1.8,
+  threshold: 0.6,
+  soft_knee: 0.7,
+
+  background_color: { r: 0, g: 0, b: 0 },
+  transparent: false
+});
+
+
+
+myFluid.activate();
+
+function splatPointer(event) {
+    const { clientX: x, clientY: y } = event;
+    const dx = (x / window.innerWidth) * 2 - 1;
+    const dy = (y / window.innerHeight) * 2 - 1;
+    myFluid.splat(x, y, dx, dy, 400);
+}
+
+window.addEventListener('pointermove', splatPointer);
+
+//mouse poiniter script code ends 
+
+
+
 const blob = document.getElementById("blob");
 
 window.onpointermove = event => { 
@@ -35,7 +89,7 @@ document.querySelector("h1").onmouseover = event => {
     }
     
     iteration += 1 / 3;
-  }, 30);
+  }, 10);
 }
 
 // Scroll animation for sections
@@ -56,3 +110,12 @@ const observer = new IntersectionObserver((entries, observer) => {
 sections.forEach(section => {
   observer.observe(section);
 });
+
+
+
+
+
+
+
+
+
